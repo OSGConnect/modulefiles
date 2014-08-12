6cbe11e35b9982aad367e30d7cf98b34bf1cd6f3
Suchandra Thapa <sthapa@ci.uchicago.edu>
2014-08-12 11:07:38 -0500

    Improve help message for ATLAS library module

--- modulefiles/Core/atlas.lua
+++ modulefiles/Core/atlas.lua
@@ -1,8 +1,10 @@
 help(
 [[
-This module loads ATLAS 3.10.1
-
+This module loads the ATLAS 3.10.1 libraries.  ATLAS is a library that
+provides support for linear algebra operations.
 ]])
+
+whatis("Loads ATLAS 3.10.1 libraries for linear algebra operations")
 
 local version = "3.10.1"
 local base = "/cvmfs/oasis.opensciencegrid.org/osg/modules/atlas/"..version
