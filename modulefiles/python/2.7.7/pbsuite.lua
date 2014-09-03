b38bff67dedf43ce3b5cd99d84cc635b6ad35454
Suchandra Thapa <sthapa@ci.uchicago.edu>
2014-09-03 15:21:52 -0500

    Add samtools and pyenv for pbsuite

---
+++ modulefiles/python/2.7.7/pbsuite.lua
@@ -0,0 +1,20 @@
+help(
+[[
+This module loads Python 2.7.7 with a set of packages for needed for the 
+PBSuite (numpy, h5py, pysam)
+
+]])
+
+local version = "2.7.7"
+local base = "/cvmfs/oasis.opensciencegrid.org/osg/modules/virtualenv-2.7/pbsuite"
+
+pushenv("VIRTUAL_ENV", base)
+prepend_path("PATH", pathJoin(base, "bin"))
+pushenv("PYTHONHOME", base)
+
+prereq('atlas', 'lapack')
+-- Setup Modulepath for packages built by this python stack
+local mroot = os.getenv("MODULEPATH_ROOT")
+local mdir = pathJoin(mroot,"Python",version)
+prepend_path("MODULEPATH", mdir)
+
