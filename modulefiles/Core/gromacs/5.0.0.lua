b14cfda3ca46ef740f897a8b54bd79c006490ccb
Suchandra Thapa <sthapa@ci.uchicago.edu>
2014-09-03 13:34:45 -0500

    Add gromacs 5.0.0 modulefile

---
+++ modulefiles/Core/gromacs/5.0.0.lua
@@ -0,0 +1,17 @@
+help(
+[[
+This module loads Gromacs 5.0.0 into the environment. Gromacs is a 
+molecular dynamics application.
+
+]])
+
+whatis("Loads the Gromacs molecular dynamics simulator")
+
+local version = "5.0.0"
+local base = "/cvmfs/oasis.opensciencegrid.org/osg/modules/gromacs/"..version
+
+prepend_path("PATH", pathJoin(base, "bin"))
+prepend_path("LD_LIBRARY_PATH", pathJoin(base, "lib"))
+
+family('gromacs')
+prereq('fftw/3.3.4-gromacs', 'atlas', 'lapack')
