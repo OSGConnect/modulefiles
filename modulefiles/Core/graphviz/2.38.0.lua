ea83a8c0326b1506cd4b0237998f5bf526eb8339
Suchandra Thapa <sthapa@ci.uchicago.edu>
2014-09-11 17:16:11 -0500

    Fix version number

--- modulefiles/Core/graphviz/2.38.0.lua
+++ modulefiles/Core/graphviz/2.38.0.lua
@@ -1,13 +1,13 @@
 help(
 [[
-This module loads Graphviz 2.3.8 into the environment. Graphviz is an
+This module loads Graphviz 2.38.0 into the environment. Graphviz is an
 application for visualizing graph data.
 
 ]])
 
 whatis("Loads the Graphviz visualization software")
 
-local version = "2.3.8"
+local version = "2.38.0"
 local base = "/cvmfs/oasis.opensciencegrid.org/osg/modules/graphviz/"..version
 
 prepend_path("PATH", pathJoin(base, "bin"))
