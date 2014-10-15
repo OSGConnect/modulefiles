bd929857a36813dd4932f0b3a3813c321e350ae1
Suchandra Thapa <sthapa@ci.uchicago.edu>
2014-10-15 16:21:11 -0500

    Use right column in MongoDB aggregation Fix cut & paste typo resulting in wrong argument being used in format call

--- tracking/reporting/generate_email_report.py
+++ tracking/reporting/generate_email_report.py
@@ -38,8 +38,6 @@ def get_week_start(date=None):
         week_start = start_date - datetime.timedelta(days=week_day)
     else:
         week_start = start_date
-    print start_date
-    print week_start
     return week_start
 
 def get_mongodb_client():
@@ -90,7 +88,7 @@ def get_top_modules(start_date, top=None):
 
     for record in db.weekly_count.aggregate([{"$match": {"date": start_date.isoformat()}},
                                              {"$group": {"_id": "$module",
-                                                         "sum": {"$sum": 1}}},
+                                                         "sum": {"$sum": "$count"}}},
                                              {"$sort": {"sum": -1}},
                                              {"$limit": top}])['result']:
 
@@ -108,7 +106,7 @@ def get_top_sites(start_date, top=None):
     site_list = []
     for record in db.weekly_count.aggregate([{"$match": {"date": start_date.isoformat()}},
                                              {"$group": {"_id": "$site",
-                                                         "sum": {"$sum": 1}}},
+                                                         "sum": {"$sum": "$count"}}},
                                              {"$sort": {"sum": -1}},
                                              {"$limit": top}])['result']:
         site_list.append((record['_id'], record['sum']))
@@ -144,30 +142,30 @@ def generate_report(start_date, end_date, email=False):
 
     report_text += "{0:-^80}\n".format(' Top 10 modules used')
     report_text += "\n\n"
-    report_text += "|{0:^30}|{1:^30}|".format('Module', '# of times used')
+    report_text += "|{0:^30}|{1:^30}|\n".format('Module', '# of times used')
     module_list = get_top_modules(start_date)
     for module, count in module_list:
-        report_text += "|{0:^30}|{1:^30}|".format(module, count)
+        report_text += "|{0:^30}|{1:^30}|\n".format(module, count)
     report_text += "\n\n"
 
     report_text += "{0:-^80}\n".format(' Top 10 sites used')
     report_text += "\n\n"
-    report_text += "|{0:^30}|{1:^30}|".format('Site', '# of times used')
+    report_text += "|{0:^30}|{1:^30}|\n".format('Site', '# of times used')
     site_list = get_top_sites(start_date)
     for site, count in site_list:
-        report_text += "|{0:^30}|{1:^30}|".format(site, count)
+        report_text += "|{0:^30}|{1:^30}|\n".format(site, count)
     report_text += "\n\n"
 
 
     report_text += "{0:-^80}\n".format(' Top 10 modules used at each site')
     report_text += "\n\n"
-    report_text += "|{0:^20}|{1:^20}|{0:^20}|".format('Site', 'Module', '# of times used')
+    report_text += "|{0:^20}|{1:^20}|{2:^20}|\n".format('Site', 'Module', '# of times used')
     site_module_list = get_site_modulelist(start_date)
     sites = site_module_list.keys()
     sites.sort()
     for site in sites:
-        for module, count in sites[site]:
-            report_text += "|{0:^20}|{1:^20}|{0:^20}|".format(site, module, count)
+        for module, count in site_module_list[site]:
+            report_text += "|{0:^20}|{1:^20}|{2:^20}|\n".format(site, module, count)
     report_text += "\n\n"
 
     if email:
