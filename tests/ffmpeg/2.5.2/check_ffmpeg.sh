100e4868eda2f34d84e042e8f88dec8e96778f84
Suchandra Thapa <sthapa@ci.uchicago.edu>
2014-12-31 14:25:08 -0600

    Use output from the right version of ffmpeg

--- tests/ffmpeg/2.5.2/check_ffmpeg.sh
+++ tests/ffmpeg/2.5.2/check_ffmpeg.sh
@@ -9,17 +9,18 @@ echo "GWMS Entry Name: $GLIDEIN_Entry_Name"
 echo "GWMS Resource Name: $GLIDEIN_ResourceName"
 
 
-FFMPEG_OUTPUT="ffmpeg version 0.10.15
-built on Dec 30 2014 10:26:28 with gcc 4.4.7 20120313 (Red Hat 4.4.7-3)
+FFMPEG_OUTPUT="ffmpeg version 2.5.2 Copyright (c) 2000-2014 the FFmpeg
+developers
+built on Dec 29 2014 16:19:47 with gcc 4.4.7 (GCC) 20120313 (Red Hat 4.4.7-3)
 configuration:
---prefix=/cvmfs/oasis.opensciencegrid.org/osg/modules/ffmpeg/0.10.15
-libavutil      51. 35.100 / 51. 35.100
-libavcodec     53. 61.100 / 53. 61.100
-libavformat    53. 32.100 / 53. 32.100
-libavdevice    53.  4.100 / 53.  4.100
-libavfilter     2. 61.100 /  2. 61.100
-libswscale      2.  1.100 /  2.  1.100
-libswresample   0.  6.100 /  0.  6.100"
+--prefix=/cvmfs/oasis.opensciencegrid.org/osg/modules/ffmpeg/2.5.2
+libavutil      54. 15.100 / 54. 15.100
+libavcodec     56. 13.100 / 56. 13.100
+libavformat    56. 15.102 / 56. 15.102
+libavdevice    56.  3.100 / 56.  3.100
+libavfilter     5.  2.103 /  5.  2.103
+libswscale      3.  1.101 /  3.  1.101
+libswresample   1.  1.100 /  1.  1.100"
 FFMPEG_OUTPUT=`echo $FFMPEG_OUTPUT | perl -pe 's/\s+/ /g'`
 source /cvmfs/oasis.opensciencegrid.org/osg/modules/lmod/5.6.2/init/bash
 module load ffmpeg/2.5.2
