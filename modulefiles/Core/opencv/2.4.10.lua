ec7fe12313cc96e5521bf8c56e0bd3b53aedbbea
Suchandra Thapa <sthapa@ci.uchicago.edu>
2015-01-09 14:40:55 -0600

    Add opencv module and update gnome-lib modulefile

---
+++ modulefiles/Core/opencv/2.4.10.lua
@@ -0,0 +1,17 @@
+help(
+[[
+This module loads OpenCV 2.4.10 into the environment. OpenCV is 
+an image processing library.
+
+]])
+
+whatis("Loads the OpenCV image processing library")
+
+local version = "2.4.10"
+local base = "/cvmfs/oasis.opensciencegrid.org/osg/modules/opencv/"..version
+
+prepend_path("PATH", pathJoin(base, "bin"))
+prepend_path("LD_LIBRARY_PATH", pathJoin(base, "lib"))
+
+family('opencv')
+prereq('gnome-libs', 'ffmpeg')
