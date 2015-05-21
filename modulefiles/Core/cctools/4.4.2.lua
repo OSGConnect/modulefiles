8c3d70afa1c9b1f81ef226d12fb909d67f637e0c
Mats Rynge <mats@rynge.net>
2015-05-21 16:47:21 -0700

    Added cctools

---
+++ modulefiles/Core/cctools/4.4.2.lua
@@ -0,0 +1,16 @@
+help(
+[[
+This module loads cctools 4.4.2
+
+http://ccl.cse.nd.edu/software/manuals/
+]])
+
+whatis("Loads the cctools collection")
+local version = "4.2.2"
+local base = "/cvmfs/oasis.opensciencegrid.org/osg/modules/cctools/"..version
+
+prepend_path("PATH", pathJoin(base, "bin"))
+
+family('cctools')
+
+
