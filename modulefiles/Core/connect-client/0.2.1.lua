987bbb577d58676bd26d45404316f21eb678f491
Suchandra Thapa <sthapa@ci.uchicago.edu>
2015-06-15 18:03:58 -0500

    Add Connect client modulefile

---
+++ modulefiles/Core/connect-client/0.2.1.lua
@@ -0,0 +1,17 @@
+help(
+[[
+This module loads the connect client 0.2.1 into the environment.
+The connect client is a setup of applications that allow users to 
+run jobs on the OSG Connect infrastructure from their local campus cluster.
+
+]])
+
+whatis("Loads the connect client tools")
+
+local version = "0.2.1"
+local base = "/cvmfs/oasis.opensciencegrid.org/osg/modules/connect-client/"..version
+
+prepend_path("PATH", pathJoin(base, "bin"))
+
+family('connect-client')
+prereq('python/2.7', 'gcc')
