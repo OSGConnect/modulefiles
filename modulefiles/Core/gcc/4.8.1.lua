897651a32516ae47d42bf5af2013ae2c0d0ed399
Suchandra Thapa <sthapa@ci.uchicago.edu>
2015-07-09 10:26:08 -0500

    Add gcc 4.8.1 modulefile and recipe

---
+++ modulefiles/Core/gcc/4.8.1.lua
@@ -0,0 +1,13 @@
+help(
+[[
+This module loads the GNU Compiler Collection (GCC) 4.8.1
+
+]])
+
+local version = "4.8.1"
+local base = "/cvmfs/oasis.opensciencegrid.org/osg/modules/gcc/"..version
+
+prepend_path("PATH", pathJoin(base, "bin"))
+prepend_path("LD_LIBRARY_PATH", pathJoin(base, "lib64"))
+
+family('gcc')
