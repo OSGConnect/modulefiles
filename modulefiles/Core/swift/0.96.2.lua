424cd2c9d7d50e40c3ae34761f173d8dfd84d51b
Suchandra Thapa <sthapa@ci.uchicago.edu>
2015-08-05 12:04:47 -0500

    Add module files for swift 0.96.2

--- modulefiles/Core/swift/0.96.2.lua
+++ modulefiles/Core/swift/0.96.2.lua
@@ -1,10 +1,10 @@
 help(
 [[
-Swift is a data-oriented coarse grained scripting language that supports dataset typing and mapping, dataset iteration, conditional branching, and procedural composition.
-Swift programs (or workflows) are written in a language called Swift scripts.
+Swift is a data-oriented coarse grained scripting language that supports dataset typing and 
+mapping, dataset iteration, conditional branching, and procedural composition.
 ]])
 whatis(" Swift programs (or workflows) are written in a language called Swift scripts.")
-local version = "0.94.1"
+local version = "0.96.2"
 local base = "/cvmfs/oasis.opensciencegrid.org/osg/modules/swift/"..version
 prepend_path("PATH", pathJoin(base, "bin"))
 prepend_path("LD_LIBRARY_PATH", pathJoin(base, "lib"))
