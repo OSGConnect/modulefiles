3ddc38e04b72c5ba1fdb49b8eec6321f925e71d0
Suchandra Thapa <sthapa@ci.uchicago.edu>
2015-08-06 10:27:20 -0500

    Add unixODBC libraries and modules

---
+++ modulefiles/Core/unixodbc/2.3.2.lua
@@ -0,0 +1,18 @@
+help(
+[[
+This module loads unixODBC 2.3.2
+
+The unixODBC Project goals are to develop and promote unixODBC to be the
+definitive standard for ODBC on non MS Windows platforms. 
+
+]])
+
+whatis("Loads unixODBC libraries")
+local version = "2.3.2"
+local base = "/cvmfs/oasis.opensciencegrid.org/osg/modules/unixODBC/"..version
+
+prepend_path("LD_LIBRARY_PATH", pathJoin(base, "lib"))
+prepend_path("PATH", pathJoin(base, "bin"))
+prepend_path("CPATH", base.."/include")
+prepend_path("LIBRARY_PATH", base.."/lib")
+family('unixODBC')
