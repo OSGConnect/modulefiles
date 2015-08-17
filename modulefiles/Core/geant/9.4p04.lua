0d7ea0693a8312b293bd64ca9c843d0b9c42824d
Suchandra Thapa <sthapa@ci.uchicago.edu>
2015-08-17 15:16:06 -0500

    Add new clhep and geant4 recipes/modules

---
+++ modulefiles/Core/geant/9.4p04.lua
@@ -0,0 +1,15 @@
+help(
+[[
+This module loads Geant4 9.4p04.  Geant4 is a detector simulator for HEP.
+
+]])
+
+whatis("Loads Geant4 9.4p04")
+
+local version = "9.4p04"
+local base = "/cvmfs/oasis.opensciencegrid.org/osg/modules/geant4/"..version
+
+prepend_path("LD_LIBRARY_PATH", pathJoin(base, "lib"))
+prepend_path("LIBRARY_PATH",  pathJoin(base, "lib64"))
+family('geant4')
+load('gcc/4.9.2', 'clhep/2.1.0.1')
