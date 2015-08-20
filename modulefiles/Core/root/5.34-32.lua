760492836e5301df7ef023ca2e9c9efdf2b94604
Suchandra Thapa <sthapa@ci.uchicago.edu>
2015-08-20 13:42:56 -0500

    Set root and geant4 env variables in modulefile

--- modulefiles/Core/root/5.34-32.lua
+++ modulefiles/Core/root/5.34-32.lua
@@ -9,7 +9,7 @@ whatis("Loads the ROOT data analysis environmemt")
 
 local version = "5.34-32"
 local base = "/cvmfs/oasis.opensciencegrid.org/osg/modules/root/"..version
-setenv("ROOTSYS", "/cvmfs/oasis.opensciencegrid.org/osg/modules/root/5.34-32")
+pushenv("ROOTSYS", "/cvmfs/oasis.opensciencegrid.org/osg/modules/root/5.34-32")
 prepend_path("PATH", pathJoin(base, "bin"))
 prepend_path("LD_LIBRARY_PATH", pathJoin(base, "lib/root"))
 prepend_path("CPATH",  pathJoin(base, "include"))
