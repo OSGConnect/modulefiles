93270c5d648035dd9aff84dcadcc19ad7a9db33e
Bala Desinghu <dmbala@gmail.com>
2015-11-18 15:11:50 -0600

    Create 1.9.4.lua
    ANTS 1.9.4 lua file

---
+++ modulefiles/Core/ANTS/1.9.4.lua
@@ -0,0 +1,10 @@
+help(
+[[
+ANTs is popularly considered a state-of-the-art medical image registration and segmentation toolkit. ANTs extracts information from complex datasets that include imaging (Word Cloud). 
+]])
+whatis("Loads ANTs, which stands for Advanced Normalization Tools")
+local version = "1.9.4"
+local base = "/cvmfs/oasis.opensciencegrid.org/osg/modules/ANTs/"..version
+prepend_path("PATH", pathJoin(base, "bin"))
+prepend_path("LD_LIBRARY_PATH", pathJoin(base, "lib"))
+family('ANTs')
