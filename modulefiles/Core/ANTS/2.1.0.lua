94dea977c7a56107ef048dab335b205a511afdfb
Bala Desinghu <dmbala@gmail.com>
2015-11-18 15:12:31 -0600

    Create 2.1.0.lua
    2.1.0.lua file

---
+++ modulefiles/Core/ANTS/2.1.0.lua
@@ -0,0 +1,9 @@
+help(
+[[
+ANTs is popularly considered a state-of-the-art medical image registration and segmentation toolkit. ANTs extracts information from complex datasets that include imaging (Word Cloud). 
+]])
+whatis("Loads ANTs, which stands for Advanced Normalization Tools")
+local version = "2.1.0"
+local base = "/cvmfs/oasis.opensciencegrid.org/osg/modules/ANTs/"..version
+prepend_path("PATH", pathJoin(base, "bin"))
+family('ANTs')
