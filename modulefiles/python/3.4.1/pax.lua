1c0b8560cf2a659ea87c8a591a2382461aa3e4fe
Suchandra Thapa <sthapa@ci.uchicago.edu>
2015-12-07 16:58:49 -0600

    Add PAX python modules recipe and modulefile

---
+++ modulefiles/python/3.4.1/pax.lua
@@ -0,0 +1,18 @@
+help(
+[[
+This module loads Python 3.4.1 with a set of packages needed for
+the Xenon1T PAX code
+
+]])
+
+local version = "3.4.1"
+local base = "/cvmfs/oasis.opensciencegrid.org/osg/modules/virtualenv-3.4/pax"
+
+pushenv("VIRTUAL_ENV", base)
+prepend_path("PATH", pathJoin(base, "bin"))
+
+load('atlas/3.10.1', 'lapack', 'hdf5/1.8.13', 'llvm/3.6', 'gcc/4.9.2', 'root/5.34-32-py34', 'snappy/1.1.3')
+-- Setup Modulepath for packages built by this python stack
+local mroot = os.getenv("MODULEPATH_ROOT")
+local mdir = pathJoin(mroot,"Python",version)
+prepend_path("MODULEPATH", mdir)
