e422eb5d10fadf72eeed5687404c936a0f89d1b8
Suchandra Thapa <sthapa@ci.uchicago.edu>
2015-12-07 15:04:03 -0600

    Add recipe and module for root 5.34-32 with python 3.4 support

---
+++ modulefiles/Core/root/5.34-32-py34.lua
@@ -0,0 +1,18 @@
+help(
+[[
+This module loads ROOT 5.34-32 with python 3.4 support into the environment. ROOT is a 
+physics analysis application.
+
+]])
+
+whatis("Loads the ROOT data analysis environmemt")
+
+local version = "5.34-32-py34"
+local base = "/cvmfs/oasis.opensciencegrid.org/osg/modules/root/"..version
+pushenv("ROOTSYS", base)
+prepend_path("PATH", pathJoin(base, "bin"))
+prepend_path("LD_LIBRARY_PATH", pathJoin(base, "lib/root"))
+prepend_path("CPATH",  pathJoin(base, "include"))
+prepend_path("LIBRARY_PATH",  pathJoin(base, "lib"))
+prepend_path("PYTHONPATH", pathJoin(base, "lib", "root"))
+family('root')
