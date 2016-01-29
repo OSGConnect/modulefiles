90943634034bc2987bf791cf157f36cdf83f2842
Suchandra Thapa <sthapa@ci.uchicago.edu>
2016-01-29 16:52:18 -0600

    Add stash 2.6 code

--- modulefiles/Core/stashcp/2.6.lua
+++ modulefiles/Core/stashcp/2.6.lua
@@ -1,15 +1,16 @@
 help(
 [[
-This module loads stashcp into the environment. stashcp is a user-facing
+This module loads stashcp 2.6   into the environment. stashcp is a user-facing
 front-end to XRootD caching for OSG Connect Stash
 
 ]])
 
-whatis("Loads the stashcp tool")
+whatis("Loads the stashcp 2.6 tool")
 
-local version = "1.0.0"
+local version = "2.6"
 local base = "/cvmfs/oasis.opensciencegrid.org/osg/modules/stashcp/"..version
 
 prepend_path("PATH", base)
+prepend_path("PATH", pathJoin(base, "bin"))
 
 family('stashcp')
