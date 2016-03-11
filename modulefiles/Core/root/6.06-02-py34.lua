748d47daaaa70cafce3d49064b26aef930e571fb
Suchandra Thapa <sthapa@ci.uchicago.edu>
2016-03-11 14:39:32 -0600

    Fix load in root 6.0.6 module

--- modulefiles/Core/root/6.06-02-py34.lua
+++ modulefiles/Core/root/6.06-02-py34.lua
@@ -1,5 +1,6 @@
 help(
 [[
+
 This module loads ROOT 6.06-02 with python 3.4 support into the environment. ROOT is a 
 physics analysis application.
 
@@ -15,6 +16,5 @@ prepend_path("LD_LIBRARY_PATH", pathJoin(base, "lib/root"))
 prepend_path("CPATH",  pathJoin(base, "include"))
 prepend_path("LIBRARY_PATH",  pathJoin(base, "lib"))
 prepend_path("PYTHONPATH", pathJoin(base, "lib", "root"))
-d('fftw/3.3.4-gromacs')
 load('gcc/4.9.2')
 family('root')
