d752469a0af3923dc80e5f9faf97683d449a7e62
Suchandra Thapa <sthapa@ci.uchicago.edu>
2016-03-14 14:09:19 -0500

    Load the right version of ROOT

--- modulefiles/python/3.4.1/pax/4.6.1.lua
+++ modulefiles/python/3.4.1/pax/4.6.1.lua
@@ -11,7 +11,7 @@ local base = "/cvmfs/oasis.opensciencegrid.org/osg/modules/virtualenv-3.4/pax-4.
 pushenv("VIRTUAL_ENV", base)
 prepend_path("PATH", pathJoin(base, "bin"))
 
-load('atlas/3.10.1', 'lapack', 'hdf5/1.8.13', 'llvm/3.6', 'gcc/4.9.2', 'root/5.34-32-py34', 'snappy/1.1.3', 'cblosc/1.7.1')
+load('atlas/3.10.1', 'lapack', 'hdf5/1.8.13', 'llvm/3.6', 'gcc/4.9.2', 'root/6.06-02-py34', 'snappy/1.1.3', 'cblosc/1.7.1')
 -- Setup Modulepath for packages built by this python stack
 local mroot = os.getenv("MODULEPATH_ROOT")
 local mdir = pathJoin(mroot,"Python",version)
