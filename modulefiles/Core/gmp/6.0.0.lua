29ca6561336dd1a101ac14f65672cb3f0c86652e
Suchandra Thapa <sthapa@ci.uchicago.edu>
2016-03-17 00:48:57 -0500

    Add CPATH and LIBRARY_PATH to gmp module

--- modulefiles/Core/gmp/6.0.0.lua
+++ modulefiles/Core/gmp/6.0.0.lua
@@ -10,5 +10,7 @@ local version = "6.0.0"
 local base = "/cvmfs/oasis.opensciencegrid.org/osg/modules/gmp/"..version
 
 prepend_path("LD_LIBRARY_PATH", pathJoin(base, "lib"))
+prepend_path("CPATH", base.."/include")
+prepend_path("LIBRARY_PATH", base.."/lib")
 
 family('gmp')
