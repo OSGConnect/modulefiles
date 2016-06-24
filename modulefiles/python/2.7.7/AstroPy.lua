ffce7aaca10b8329292f4b9abe82ad78643980ff
Bala Desinghu <dmbala@gmail.com>
2016-06-24 14:53:52 -0500

    Create AstroPy.lua

---
+++ modulefiles/python/2.7.7/AstroPy.lua
@@ -0,0 +1,19 @@
+help(
+[[
+This module loads Python 2.7.7 with a set of packages for needed for the 
+AstroPy (numpy)
+
+]])
+
+local version = "2.7.7"
+local base = "/cvmfs/oasis.opensciencegrid.org/osg/modules/virtualenv-2.7/AstroPy"
+
+pushenv("VIRTUAL_ENV", base)
+prepend_path("PATH", pathJoin(base, "bin"))
+pushenv("PYTHONHOME", base)
+
+load('atlas/3.10.1', 'lapack', 'hdf5/1.8.13', 'gcc/4.8.1')
+-- Setup Modulepath for packages built by this python stack
+local mroot = os.getenv("MODULEPATH_ROOT")
+local mdir = pathJoin(mroot,"Python",version)
+prepend_path("MODULEPATH", mdir)
