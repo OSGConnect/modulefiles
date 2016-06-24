7294b819395f3e03c622846269634bc3d0ad3409
Suchandra Thapa <sthapa@ci.uchicago.edu>
2016-06-24 09:46:04 -0500

    Add Rosetta 2016.02

---
+++ modulefiles/Core/rosetta/2016-02.lua
@@ -0,0 +1,16 @@
+help(
+[[
+The Rosetta software suite includes algorithms for computational modeling and
+analysis of protein structures.  
+]])
+
+whatis("Loads rosetta suit of programs")
+
+local version = "2016.02"
+local base = "/cvmfs/oasis.opensciencegrid.org/osg/modules/rosetta/"..version
+
+prepend_path("PATH", pathJoin(base, "bin"))
+prepend_path("PATH", pathJoin(base, "build"))
+prepend_path("PATH", pathJoin(base, "tools"))
+prepend_path("LD_LIBRARY_PATH", pathJoin(base, "/build/external/release/linux/2.6/64/x86/gcc/4.4/default/"))
+family('rosetta')
