a2674e82ec6c29acdc11ec13dccd8be82662e842
Suchandra Thapa <sthapa@ci.uchicago.edu>
2016-07-14 15:14:53 -0500

    Add pythonpath to rucio module

--- modulefiles/Core/rucio/1.6.6.lua
+++ modulefiles/Core/rucio/1.6.6.lua
@@ -11,6 +11,7 @@ local version = "1.6.6"
 local base = "/cvmfs/oasis.opensciencegrid.org/osg/modules/rucio/"..version
 
 prepend_path("PATH", pathJoin(base, "bin"))
+prepend_path("PYTHONPATH", pathJoin(base, "lib", "python2.6", "site-packages"))
 setenv("RUCIO_HOME", pathJoin(base, "rucio"))
 family('rucio')
 load("gfal/7.20")
