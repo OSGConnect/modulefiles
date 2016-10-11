76f740e239f8b2cc963a3329b68cc57bcf2038b4
Suchandra Thapa <sthapa@ci.uchicago.edu>
2016-10-11 14:33:09 -0500

    Add Python 3.5.2

---
+++ modulefiles/Core/python/3.5.2.lua
@@ -0,0 +1,23 @@
+help(
+[[
+This module loads a clean install of Python 3.5.2
+
+]])
+
+whatis("Load an clean installation of the Python 3.5.2 interpreter")
+
+local version = "3.5.2"
+local base = "/cvmfs/oasis.opensciencegrid.org/osg/modules/python-"..version
+
+prepend_path("PATH", pathJoin(base, "bin"))
+prepend_path("LD_LIBRARY_PATH", pathJoin(base, "lib"))
+prepend_path("CPATH",  pathJoin(base, "include"))
+prepend_path("LIBRARY_PATH",  pathJoin(base, "lib"))
+
+
+family('python')
+-- Setup Modulepath for packages built by this python stack
+local mroot = os.getenv("MODULEPATH_ROOT")
+local mdir = pathJoin(mroot,"python",version)
+prepend_path("MODULEPATH", mdir)
+
