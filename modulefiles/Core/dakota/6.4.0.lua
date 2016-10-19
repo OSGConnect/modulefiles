ec6bd0a7411a73e50a835f48008a4b3e5540a6aa
Mats Rynge <mats@rynge.net>
2016-10-19 11:01:03 -0700

    Fixed typo in dakota lua file

--- modulefiles/Core/dakota/6.4.0.lua
+++ modulefiles/Core/dakota/6.4.0.lua
@@ -2,7 +2,7 @@ help(
 [[
 The Dakota toolkit provides a flexible, extensible interface between analysis codes and iterative systems analysis methods.
 ]])
-whatis("Loads Dakota, which provides a flexible, extensible interface between analysis codes and iterative systems analysis methods."]
+whatis("Loads Dakota, which provides a flexible, extensible interface between analysis codes and iterative systems analysis methods.")
 local version = "6.4.0"
 local base = "/cvmfs/oasis.opensciencegrid.org/osg/modules/dakota/"..version
 prepend_path("PATH", pathJoin(base, "bin"))
