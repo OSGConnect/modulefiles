d26264d9691670bac96354be072d6963fef14df0
Suchandra Thapa <sthapa@ci.uchicago.edu>
2017-04-11 09:15:30 -0500

    Add clhep 2.3.4.4

---
+++ modulefiles/Core/clhep/2.3.4.4.lua
@@ -0,0 +1,21 @@
+help(
+[[
+The CLHEP project was proposed by Leif Lönnblad at CHEP 92. It is intended to be
+a set of HEP-specific foundation and utility classes such as random generators,
+physics vectors, geometry and linear algebra. CLHEP is structured in a set of
+packages independent of any external package (interdependencies within CLHEP are
+allowed under certain conditions). 
+
+]])
+
+whatis("Loads the CLHEP library which provides a library for particle physics")
+
+local version = "2.3.4.4"
+local base = "/cvmfs/oasis.opensciencegrid.org/osg/modules/clhep/"..version
+
+prepend_path("PATH",  pathJoin(base, "bin"))
+prepend_path("CPATH",  pathJoin(base, "include"))
+prepend_path("LIBRARY_PATH",  pathJoin(base, "lib"))
+prepend_path("LD_LIBRARY_PATH",  pathJoin(base, "lib"))
+pushenv("CLHEP_BASE_DIR", base)
+family('clhep')
