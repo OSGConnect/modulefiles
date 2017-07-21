900cfac32dbcc231b02fe6c5546b7eafeb671a87
Suchandra Thapa <sthapa@ci.uchicago.edu>
2017-07-21 14:39:52 -0500

    Add bin paths to module

--- modulefiles/Core/hdf5/1.8.13.lua
+++ modulefiles/Core/hdf5/1.8.13.lua
@@ -14,4 +14,5 @@ local base = "/cvmfs/oasis.opensciencegrid.org/osg/modules/hdf5/"..version
 prepend_path("LD_LIBRARY_PATH", pathJoin(base, "lib"))
 prepend_path("CPATH",  pathJoin(base, "include"))
 prepend_path("LIBRARY_PATH",  pathJoin(base, "lib"))
+prepend_path("PATH",  pathJoin(base, "bin"))
 family('hdf5')
