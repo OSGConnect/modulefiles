97d572f86d570f5b60f9adc1227437d2ffc55d47
Bala Desinghu <dmbala@gmail.com>
2017-10-27 14:45:21 -0500

    Update 2017b.lua

--- modulefiles/Core/matlab/2017b.lua
+++ modulefiles/Core/matlab/2017b.lua
@@ -6,12 +6,12 @@ MATLAB Compiler Runtime r2017b.
 This module loads a standalone set of libraries for executing MATLAB code.
 
 The MCR is freely distributable as described by the license file located at:
-  /cvmfs/oasis.opensciencegrid.org/osg/modules/matlab/2017b/v92/MCR_license.txt
+  /cvmfs/oasis.opensciencegrid.org/osg/modules/matlab/2017b/v93/MCR_license.txt
 
 ]])
 
 local version = "2017b"
-local base = "/cvmfs/oasis.opensciencegrid.org/osg/modules/matlab/"..version.."/v92"
+local base = "/cvmfs/oasis.opensciencegrid.org/osg/modules/matlab/"..version.."/v93"
 
 prepend_path("PATH", pathJoin(base, "bin"))
 prepend_path("LD_LIBRARY_PATH", pathJoin(base, "runtime/glnxa64"))
