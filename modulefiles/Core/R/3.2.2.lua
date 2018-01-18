ea7450ac5a11ab9ad7ea9c6033f00f2e5635d779
Bala Desinghu <dmbala@gmail.com>
2018-01-18 10:43:06 -0600

    Create 3.2.2.lua
    3.2.2.lua file

---
+++ modulefiles/Core/R/3.2.2.lua
@@ -0,0 +1,14 @@
+help(
+[[
+This module loads R 3.2.2 into the environment
+
+]])
+
+whatis("Loads R, a tool for statistical analysis")
+local version = "3.2.2"
+local base = "/cvmfs/oasis.opensciencegrid.org/osg/modules/R/"..version
+
+prepend_path("PATH", pathJoin(base, "bin"))
+prepend_path("LD_LIBRARY_PATH", pathJoin(base, "lib64"))
+
+family('R')
