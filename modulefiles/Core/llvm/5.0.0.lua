053819770870aaf6c1021d5450053d07b6a7456e
Bala Desinghu <dmbala@gmail.com>
2018-02-01 16:44:08 -0600

    create 5.0.0.lua for llvm

---
+++ modulefiles/Core/llvm/5.0.0.lua
@@ -0,0 +1,20 @@
+help(
+[[
+The LLVM Project is a collection of modular and reusable compiler and toolchain
+technologies.
+]])
+
+whatis("Loads LLVM into the environment")
+
+local version = "5.0.0"
+local base = "/cvmfs/oasis.opensciencegrid.org/osg/modules/llvm/"..version
+
+prepend_path("PATH", pathJoin(base, "bin"))
+prepend_path("LD_LIBRARY_PATH", pathJoin(base, "lib"))
+prepend_path("LIBRARY_PATH", pathJoin(base, "lib"))
+prepend_path("CPATH", pathJoin(base, "include"))
+
+family('llvm')
+load('gcc/6.2.0')
+load('cmake/3.8.0')
+load('python/2.7')
