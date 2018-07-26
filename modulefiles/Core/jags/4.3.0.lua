e5c21b21814ffc6af11f7e8cf887229a0cc71c1c
Suchandra Thapa <sthapa@ci.uchicago.edu>
2018-07-26 16:41:59 -0500

    Add JAGS recipe and module

---
+++ modulefiles/Core/jags/4.3.0.lua
@@ -0,0 +1,17 @@
+help(
+[[
+This module loads JAGS 4.3.0.  JAGS is a gaussian solver.
+
+]])
+
+whatis("Loads JAGS 4.3.0")
+
+local version = "4.3.0"
+local base = "/cvmfs/oasis.opensciencegrid.org/osg/modules/jags/"..version
+
+prepend_path("PATH",  pathJoin(base, "bin"))
+prepend_path("CPATH",  pathJoin(base, "include"))
+prepend_path("LIBRARY_PATH",  pathJoin(base, "lib"))
+prepend_path("LD_LIBRARY_PATH",  pathJoin(base, "lib"))
+
+family('jags')
