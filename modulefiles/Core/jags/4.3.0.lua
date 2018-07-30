c8700950af4589ba2f02dd44712d3729ddbab206
Suchandra Thapa <sthapa@ci.uchicago.edu>
2018-07-30 17:40:04 -0500

    Load lapack

--- modulefiles/Core/jags/4.3.0.lua
+++ modulefiles/Core/jags/4.3.0.lua
@@ -14,4 +14,5 @@ prepend_path("CPATH",  pathJoin(base, "include"))
 prepend_path("LIBRARY_PATH",  pathJoin(base, "lib"))
 prepend_path("LD_LIBRARY_PATH",  pathJoin(base, "lib"))
 
+load('lapack/3.6.1')
 family('jags')
